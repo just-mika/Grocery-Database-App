1d690ab1a4a26fc2c522f9bd3b959d2378a9646b
just-mika <mikaelaamon@gmail.com>
2024-10-27 23:24:09 +0800

    Added files

---
+++ sql/grocery_database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE grocery_database;
+
+USE grocery_database;
+
+-- CREATE TABLE inventory
+-- CREATE TABLE customer
+-- CREATE TABLE sales
+-- CREATE TABLE product
+-- create other necessary tables
