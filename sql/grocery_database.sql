e13286704ec8e858e6c5f0cbbce8d2ef8782198d
just-mika <mikaelaamon@gmail.com>
2024-11-17 18:20:16 +0800

    updated

--- sql/grocery_database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE grocery_database;
-
-USE grocery_database;
-
--- CREATE TABLE inventory
--- CREATE TABLE customer
--- CREATE TABLE sales
--- CREATE TABLE product
--- create other necessary tables
