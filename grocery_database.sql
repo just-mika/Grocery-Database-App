f5467cbf8446d38a4bff8850bfddd3bf59588058
HannahCorpuz <134398339+HannahCorpuz@users.noreply.github.com>
2024-11-17 21:08:41 +0800

    Add files via upload

--- grocery_database.sql
+++ grocery_database.sql
@@ -127,6 +127,30 @@ LOCK TABLES `sales` WRITE;
 /*!40000 ALTER TABLE `sales` DISABLE KEYS */;
 /*!40000 ALTER TABLE `sales` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `securitycheck`
+--
+
+DROP TABLE IF EXISTS `securitycheck`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `securitycheck` (
+  `idsecuritycheck` int NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idsecuritycheck`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `securitycheck`
+--
+
+LOCK TABLES `securitycheck` WRITE;
+/*!40000 ALTER TABLE `securitycheck` DISABLE KEYS */;
+INSERT INTO `securitycheck` VALUES (1,'123456');
+/*!40000 ALTER TABLE `securitycheck` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -137,4 +161,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-31 10:33:42
+-- Dump completed on 2024-11-17 20:19:00
