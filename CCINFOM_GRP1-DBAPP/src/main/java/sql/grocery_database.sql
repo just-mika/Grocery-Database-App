7b1187cb5747485a80f665d0bf7f0689d78b422b
just-mika <mikaelaamon@gmail.com>
2024-11-18 16:18:20 +0800

    updated database

--- CCINFOM_GRP1-DBAPP/src/main/java/sql/grocery_database.sql
+++ CCINFOM_GRP1-DBAPP/src/main/java/sql/grocery_database.sql
@@ -1,164 +1,188 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: localhost    Database: grocery_database
--- ------------------------------------------------------
--- Server version	8.0.39
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
---
--- Table structure for table `customers`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema grocery_database
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `grocery_database` ;
 
-DROP TABLE IF EXISTS `customers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `customers` (
-  `customerUID` int NOT NULL,
-  `lastName` varchar(45) DEFAULT NULL,
-  `firstName` varchar(45) DEFAULT NULL,
-  `phoneNumber` bigint DEFAULT NULL,
-  `transactionID` int DEFAULT NULL,
-  PRIMARY KEY (`customerUID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Schema grocery_database
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `grocery_database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `grocery_database` ;
 
---
--- Dumping data for table `customers`
---
+-- -----------------------------------------------------
+-- Table `grocery_database`.`customer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `grocery_database`.`customer` ;
 
-LOCK TABLES `customers` WRITE;
-/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE IF NOT EXISTS `grocery_database`.`customer` (
+  `customer_id` INT NOT NULL,
+  `last_name` VARCHAR(45) NULL DEFAULT NULL,
+  `first_name` VARCHAR(45) NULL DEFAULT NULL,
+  `contact_number` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`customer_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `inventory`
---
 
-DROP TABLE IF EXISTS `inventory`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `inventory` (
-  `inventoryID` int NOT NULL,
-  `productID` int DEFAULT NULL,
-  `stockInWarehouse` int DEFAULT NULL,
-  `stockStatus` enum('O','L','F') DEFAULT NULL,
-  `Inventorycol` varchar(45) DEFAULT NULL,
-  `type` enum('F','B','T','H','M') DEFAULT NULL,
-  `expiry` datetime DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  PRIMARY KEY (`inventoryID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Table `grocery_database`.`product_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `grocery_database`.`product_info` ;
 
---
--- Dumping data for table `inventory`
---
+CREATE TABLE IF NOT EXISTS `grocery_database`.`product_info` (
+  `product_code` INT NOT NULL,
+  `product_name` VARCHAR(45) NULL DEFAULT NULL,
+  `price` FLOAT NOT NULL DEFAULT '0',
+  `product_category` VARCHAR(45) NULL DEFAULT NULL,
+  `quantity_in_stock` INT NOT NULL DEFAULT '0',
+  PRIMARY KEY (`product_code`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-LOCK TABLES `inventory` WRITE;
-/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
-/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `products`
---
+-- -----------------------------------------------------
+-- Table `grocery_database`.`sales_transactions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `grocery_database`.`sales_transactions` ;
 
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `products` (
-  `productCode` int NOT NULL,
-  `productName` varchar(45) DEFAULT NULL,
-  `description` varchar(45) DEFAULT NULL,
-  `stockQuantity` int DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `expiry` date DEFAULT NULL,
-  `brand` varchar(45) DEFAULT NULL,
-  `category` varchar(45) DEFAULT NULL,
-  `allergy` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`productCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE IF NOT EXISTS `grocery_database`.`sales_transactions` (
+  `transaction_id` INT NOT NULL,
+  `total_items` INT NOT NULL DEFAULT '0',
+  `total_cost` FLOAT NOT NULL DEFAULT '0',
+  `date_of_sale` DATETIME NULL DEFAULT NULL,
+  `customer_id` INT NOT NULL,
+  PRIMARY KEY (`transaction_id`),
+  INDEX `fk_sales_transactions_customer1_idx` (`customer_id` ASC) VISIBLE,
+  CONSTRAINT `fk_sales_transactions_customer1`
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `grocery_database`.`customer` (`customer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `products`
---
 
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
+-- -----------------------------------------------------
+-- Table `grocery_database`.`customer_items`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `grocery_database`.`customer_items` ;
 
---
--- Table structure for table `sales`
---
+CREATE TABLE IF NOT EXISTS `grocery_database`.`customer_items` (
+  `item_id` INT NOT NULL,
+  `quantity` INT NOT NULL DEFAULT '0',
+  `product_total` FLOAT NOT NULL DEFAULT 0.0,
+  `price` FLOAT NOT NULL DEFAULT '0',
+  `product_code` INT NOT NULL,
+  `sales_transactions_transaction_id` INT NOT NULL,
+  PRIMARY KEY (`item_id`),
+  INDEX `fk_customer_items_product_info1_idx` (`product_code` ASC) VISIBLE,
+  INDEX `fk_customer_items_sales_transactions1_idx` (`sales_transactions_transaction_id` ASC) VISIBLE,
+  CONSTRAINT `fk_customer_items_product_info1`
+    FOREIGN KEY (`product_code`)
+    REFERENCES `grocery_database`.`product_info` (`product_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_customer_items_sales_transactions1`
+    FOREIGN KEY (`sales_transactions_transaction_id`)
+    REFERENCES `grocery_database`.`sales_transactions` (`transaction_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `sales`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `sales` (
-  `salesID` int NOT NULL,
-  `ORID` int DEFAULT NULL,
-  `itemBought` varchar(45) DEFAULT NULL,
-  `quantity` int DEFAULT NULL,
-  `additionalTaxes` double DEFAULT NULL,
-  `damgesOrLosses` double DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  PRIMARY KEY (`salesID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `sales`
---
+-- -----------------------------------------------------
+-- Table `grocery_database`.`payment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `grocery_database`.`payment` ;
 
-LOCK TABLES `sales` WRITE;
-/*!40000 ALTER TABLE `sales` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sales` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE IF NOT EXISTS `grocery_database`.`payment` (
+  `payment_id` INT NOT NULL,
+  `payment_type` VARCHAR(45) NULL,
+  `transaction_id` INT NOT NULL,
+  PRIMARY KEY (`payment_id`),
+  INDEX `fk_payment_sales_transactions_idx` (`transaction_id` ASC) VISIBLE,
+  CONSTRAINT `fk_payment_sales_transactions`
+    FOREIGN KEY (`transaction_id`)
+    REFERENCES `grocery_database`.`sales_transactions` (`transaction_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `securitycheck`
---
 
-DROP TABLE IF EXISTS `securitycheck`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `securitycheck` (
-  `idsecuritycheck` int NOT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idsecuritycheck`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Table `grocery_database`.`supplier`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `grocery_database`.`supplier` ;
 
---
--- Dumping data for table `securitycheck`
---
+CREATE TABLE IF NOT EXISTS `grocery_database`.`supplier` (
+  `supplier_code` INT NOT NULL,
+  `supplier_name` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`supplier_code`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-LOCK TABLES `securitycheck` WRITE;
-/*!40000 ALTER TABLE `securitycheck` DISABLE KEYS */;
-INSERT INTO `securitycheck` VALUES (1,'123456');
-/*!40000 ALTER TABLE `securitycheck` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- -----------------------------------------------------
+-- Table `grocery_database`.`product_order_batch`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `grocery_database`.`product_order_batch` ;
 
--- Dump completed on 2024-11-17 20:19:00
+CREATE TABLE IF NOT EXISTS `grocery_database`.`product_order_batch` (
+  `batch_id` INT UNSIGNED NOT NULL,
+  `quantity_ordered` INT UNSIGNED NOT NULL DEFAULT '0',
+  `date_ordered` DATETIME NULL DEFAULT NULL,
+  `cost` FLOAT UNSIGNED NOT NULL DEFAULT '0',
+  `product_code` INT NOT NULL,
+  `supplier_code` INT NOT NULL,
+  PRIMARY KEY (`batch_id`),
+  INDEX `fk_product_order_batch_product_info1_idx` (`product_code` ASC) VISIBLE,
+  INDEX `fk_product_order_batch_supplier1_idx` (`supplier_code` ASC) VISIBLE,
+  CONSTRAINT `fk_product_order_batch_product_info1`
+    FOREIGN KEY (`product_code`)
+    REFERENCES `grocery_database`.`product_info` (`product_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_product_order_batch_supplier1`
+    FOREIGN KEY (`supplier_code`)
+    REFERENCES `grocery_database`.`supplier` (`supplier_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `grocery_database`.`securitycheck`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `grocery_database`.`securitycheck` ;
+
+CREATE TABLE IF NOT EXISTS `grocery_database`.`securitycheck` (
+  `idsecuritycheck` INT NOT NULL,
+  `password` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idsecuritycheck`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
